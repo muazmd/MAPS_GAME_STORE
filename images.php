00c93a84d552cab464992065d79ca03c865ac122
muazmd <80607495+md1999-m@users.noreply.github.com>
2021-05-27 19:04:28 +0300

    d

--- images.php
+++ images.php
@@ -27,7 +27,7 @@ if (isset($_POST["upload"])){
             $price = $_POST['price'];
             $genre =$_POST['genre'];
             
-         
+            echo $name, $price, $genre, $imgContent;
             // Insert image content into database 
             $insert = $db->query("INSERT into games (name,image,price,genre) VALUES ('$name','$imgContent','$price','$genre')"); 
              
